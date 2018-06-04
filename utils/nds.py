2797aa7a08beda01218153b5723be4bbf8c49463
vgmoose <me@vgmoose.com>
2018-06-04 03:13:54 -0400

    nds: add full .nds file dumping and reading

--- utils/nds.py
+++ utils/nds.py
@@ -1,27 +1,94 @@
-import struct
+import struct, sys, os
+
+DUMP_DIR = "extracted"
+
+def mkdir(target):
+    try:
+        os.makedirs(target)
+    except OSError:
+        pass # already exists / error
 
 class File(object):
     def __init__(self, id, name):
         self.id = id
         self.name = name
+        self.is_dir = False
 
     def __repr__(self):
-        return "\"%s\"" % self.name
+        return "\"%s [%d]\"" % (self.name, self.id)
+
+    def cache(self, parent_path):
+        self.path = parent_path + "/" + self.name
+        path_data[self.path] = self
+
+        if is_dir:
+            self.name = "[" + self.name + "]"   # add [] for dirs
+
+    def extract(self):
+        print("Extracting %s to %s%s" % (self.name, DUMP_DIR, self.path))
+        mkdir(os.path.dirname(self.path))
+
+        # read file size info
+        size_info_pos = fat_pos + 8*self.id
+        start = ri(size_info_pos)
+        end = ri(size_info_pos+4)
+
+        # # size of file to extract
+		# size = end - start
+
+        # create file and write bytes to it
+        out = open(DUMP_DIR + self.path, "wb")
+        out.write(data[start:end])
+        out.close()
 
 class Folder(File):
-    def __init__(self, id, name):
+    def __init__(self, id=0, name=""):
         super(Folder, self).__init__(id, name)
         self.contents = []
+        self.is_dir = True
+        self.path = ""
+
     def add(self, file):
         self.contents.append(file)
 
+    def tree(self, indent=0):
+        s = (indent*" ") + self.name + "\n"
+        indent += 2
+        for child in self.contents:
+            # if this child is a folder, go deeper
+            if child.is_dir:
+                s += child.tree(indent)
+            else:
+                s += (indent*" ") + child.name + "\n"
+        return s
+
+    def extract(self):
+        # make a local folder for this NDS folder's path
+        mkdir(DUMP_DIR + self.path)
+
+        # go through children, extract them too
+        for child in self.contents:
+            child.extract()
+
 # open nds file
+parsed_args = [x for x in sys.argv if not x.startswith("-")]
+if len(parsed_args) <= 1:
+    print("Please specify NDS rom file")
+    print("Usage: python nds.py [-l] [-e <path_to_extract>] <file.nds>")
+    print("       python nds.py -l <file.nds>")
+    print("       python nds.py -e <path_to_extract> <file.nds>")
+    exit(1)
+
 data = None
-with open("p.nds", "rb") as contents:
-    data = contents.read()
+try:
+    with open(parsed_args[1], "rb") as contents:
+        data = contents.read()
+except IOError:
+    print("Error: Could not open file \"%s\"" % parsed_args[1])
+    exit(4)
 
 if not data:
-    print("File 'p.nds' not found")
+    print("File is empty or bad permissions?")
 
 title = data[:12]
 code = data[12:16]
@@ -54,24 +121,30 @@ print("File Allocation Table (FAT) info:")
 print("  offset: %d" % fat_pos)
 print("  length: %d" % fat_len)
 
-root = Folder(0, "/")
+root = Folder(0, "[/]")
 process = []
 process.append(root)
 
-while process:
-    print process
-    cur_dir = process.pop()
+# map of every file to File/Folder object
+path_data = {"/": root}
 
-    cur_pos = fnt_pos + 8*(cur_dir.id & 0xFFF)
+# as long as the folders list to process isn't empty
+while process:
+    cur_dir = process.pop(0)
+
+    cur_pos = fnt_pos + 8*(cur_dir.id & 0xfff)
 
     entry_start = ri(cur_pos)
     top_file_id = ri2(cur_pos+4)
 
+    # the top file ID for this directory's files to count from
     file_id = top_file_id
 
     # go through entries at this cur_dir
-    entry_pos = cur_pos + entry_start
+    entry_pos = fnt_pos + entry_start
+
     while True:
+        # flags for current entry
         flags = rb(entry_pos)
 
         name_len = flags & 0x7f
@@ -82,19 +155,62 @@ while process:
             # break and move onto any next dirs to process
             break
 
+        # name of this folder/file
         name = rs(entry_pos+1, name_len)
-        print name
 
         if is_dir:
-            entry = Folder(file_id, name)
+            # directories read their ID from disk
+            dir_id = ri2(entry_pos+len(name)+1) & 0xfff
+
+            # create a new folder entry
+            entry = Folder(dir_id, name)
+
             # add for processing later
             process.append(entry)
+
+            # move over by 2 more
+            entry_pos += 2
         else:
+            # files are given the current file_id
             entry = File(file_id, name)
 
         # add it to the current directory
         cur_dir.add(entry)
 
+        # cache it for lookup up by path later
+        entry.cache(cur_dir.path)
+
         # increment to the next file ID
         file_id += 1
-        entry_pos += len(name) + 3    # next entry
+
+        # next entry to process
+        entry_pos += len(name) + 1
+
+# do argument parsing operations
+if "-l" in sys.argv:
+    # print out file tree
+    print("Listing file contents:")
+    print(root.tree())
+
+if "-e" in sys.argv:
+    arg_pos = sys.argv.index("-e") + 1
+    if arg_pos >= len(sys.argv):
+        print("-e requires a path to extract as an argument")
+    else:
+        extract_path = sys.argv[arg_pos]
+        if extract_path != "/" and extract_path.endswith("/"):
+            extract_path = extract_path.rstrip("/")
+        # lookup entry from extraction path
+        if extract_path not in path_data:
+            print("Could not find path \"%s\" in NDS contents" % extract_path)
+        else:
+            entry = path_data[extract_path]
+            mkdir(DUMP_DIR)
+            # extract the files from this entry
+            entry.extract()
+
+            print("Extracted file(s) to \"./%s\" folder" % DUMP_DIR)
+
+if len(parsed_args) == len(sys.argv):
+    print("No commands specified, try -l or -e <path_to_extract>")
+    exit(3)
