63ea073d6028ccb57d2e7b9e069947af4a75dd7d
vgmoose <me@vgmoose.com>
2018-06-20 23:58:06 -0400

    narc: partial writing

--- pypokescript/games/utils/narc.py
+++ pypokescript/games/utils/narc.py
@@ -7,9 +7,20 @@ def ri(data, pos, size=4, type="I"):
 def ri4(data, pos):
     return ri(data, pos, 4, "I")
 
+# write functions (little endian)
+def wi(data, pos, message, size=4, type="I"):
+    write = struct.pack("<"+type, message)
+    data[pos:pos+size] = write
+
 class NARC:
     def __init__(self, nds, path):
         # nds is a NDS file, and path is the desired path that points to the narc file within it
+
+        # the layout of a narc is as follows:
+        # NARC
+        #   - btaf (file start, end positions)
+        #   - btnf (directory structure)
+        #   - gmif (actual file contents)
 
         data = nds.path_data[path].data
 
@@ -19,6 +30,9 @@ class NARC:
         self.data = data
 
         btaf_pos = 0x10
+
+        # stash the btaf_pos for later
+        self.btaf_pos = btaf_pos
 
         # find btaf size
         btaf_size = ri4(data, btaf_pos + 4)
@@ -36,8 +50,14 @@ class NARC:
 
         offset = btaf_pos + btaf_size
 
-        # offset by bntf size (first word in bntf header after magic)
-        offset += ri(data, offset + 4)
+        btnf_size = ri(data, offset + 4)
+
+        # save the btnf to just spit out later
+        # (actually we don't have to do this, since we'll overwrite it)
+        # self.btnf_data = data[]offset:offset+btnf_size]
+
+        # offset by btnf size (first word in btnf header after magic)
+        offset += btnf_size
 
         # let's make sure we're where we think we are (gmif magic)
         if data[offset:offset+4].decode() != "GMIF":
@@ -46,8 +66,31 @@ class NARC:
         # gmif_body will now be pointing at the first byte in gmif header + 4, (after magic)
         gmif_body = offset + 8
 
+        # save for later
+        self.gmif_body = gmif_body
+
         # the data files will be read from start, end pairings
         for loc in file_locs:
             self.files.append(data[gmif_body+loc[0]:gmif_body+loc[1]])
 
         # all files loaded into memory
+
+    def save(self):
+        # start at the btaf header plus size and entries (these won't change)
+        offset = self.btaf_pos + 8
+
+        new_offset = 0
+
+        # go through our files, and write our offsets out
+        # (they're going to be relative to the gmif body)
+        for cur_file in range(self.files):
+            wi(data, offset, new_offset)
+            new_offset += len(cur_file)
+            wi(data, offset+4, new_offset)
+
+            offset += 8
+
+        # we can leave the btnf section alone
+
+        # just go through and write the data out for every file contiguously
+        
